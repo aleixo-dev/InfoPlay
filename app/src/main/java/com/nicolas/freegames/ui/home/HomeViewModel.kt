879c597c068dfc08e69aa792d4552181d60b8a41
Aleixo-Dev <nicolasaleixo2020@gmail.com>
2023-04-14 10:36:08 -0300

    refactor: adjust home state

--- app/src/main/java/com/nicolas/freegames/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/nicolas/freegames/ui/home/HomeViewModel.kt
@@ -3,9 +3,12 @@ package com.nicolas.freegames.ui.home
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.nicolas.freegames.data.repository.FreeGameRepository
-import com.nicolas.freegames.models.domain.ModelGame
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -14,8 +17,8 @@ class HomeViewModel @Inject constructor(
     private val repository: FreeGameRepository,
 ) : ViewModel() {
 
-    private val _uiState = MutableStateFlow(GameStateUi())
-    val uiState: StateFlow<GameStateUi> = _uiState.asStateFlow()
+    private val _uiState = MutableStateFlow<HomeUiState>(HomeUiState.Idle)
+    val uiState: StateFlow<HomeUiState> = _uiState.asStateFlow()
 
     fun onEvent(event: GameUiEvent) {
         when (event) {
@@ -30,31 +33,20 @@ class HomeViewModel @Inject constructor(
 
     private fun getCategory(category: String) = viewModelScope.launch {
         repository.getGamesPerCategory(category)
-            .onStart { setStateGameUi(isLoading = true) }
-            .catch { setStateGameUi(hasError = true) }
-            .collect { data -> setStateGameUi(data = data, loaded = true) }
+            .onStart { setState(HomeUiState.Loading) }
+            .catch { setState(HomeUiState.Error(it.message.toString())) }
+            .collect { data -> setState(newState = HomeUiState.Success(data = data)) }
     }
 
     private fun getAllGames() = viewModelScope.launch {
         repository.getAllGames()
-            .onStart { setStateGameUi(isLoading = true ) }
-            .catch { setStateGameUi(hasError = true) }
-            .collect { data -> setStateGameUi(data = data, loaded = true) }
+            .onStart { setState(HomeUiState.Loading) }
+            .catch { setState(HomeUiState.Error(it.message.toString())) }
+            .collect { data -> setState(newState = HomeUiState.Success(data = data)) }
     }
 
-    private fun setStateGameUi(
-        isLoading: Boolean = false,
-        data: List<ModelGame> = listOf(),
-        hasError: Boolean = false,
-        loaded: Boolean = false
-    ) {
-        _uiState.update { currentState ->
-            currentState.copy(
-                isLoading = isLoading,
-                data = data,
-                hasError = hasError,
-                loaded = loaded
-            )
-        }
+
+    private fun setState(newState: HomeUiState) {
+        _uiState.value = newState
     }
 }
