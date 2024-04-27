a973fc82d2244412cee05f4f922fcb8639ed771b
Aleixo-Dev <nicolasaleixo2020@gmail.com>
2024-04-27 12:31:54 -0300

    refactor: adjust card game

--- app/src/main/java/com/nicolas/freegames/ui/home/components/CardGame.kt
+++ app/src/main/java/com/nicolas/freegames/ui/home/components/CardGame.kt
@@ -1,5 +1,6 @@
 package com.nicolas.freegames.ui.home.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,22 +10,19 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.compose.currentBackStackEntryAsState
-import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
+import coil.size.Size
 import com.nicolas.freegames.models.domain.ModelGame
 import com.nicolas.freegames.navigation.Screen
 
@@ -38,17 +36,22 @@ fun CardGame(game: ModelGame, navController: NavController) {
             navController.navigate(route = "${Screen.DETAILS.name}/${game.id}")
         }
     ) {
-        AsyncImage(
+
+        val painter = rememberAsyncImagePainter(
             model = ImageRequest.Builder(LocalContext.current)
                 .data(game.thumbnail)
-                .crossfade(true)
-                .build(),
-            modifier = Modifier
+                .size(Size.ORIGINAL)
+                .build()
+        )
+
+        Image(
+            painter = painter, modifier = Modifier
                 .size(width = 145.dp, height = 85.dp)
                 .clip(RoundedCornerShape(5.dp)),
             contentScale = ContentScale.FillBounds,
             contentDescription = "thumbnail_game"
         )
+
         Text(
             text = game.title ?: "",
             maxLines = 1,
