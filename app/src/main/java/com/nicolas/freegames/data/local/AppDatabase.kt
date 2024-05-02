0765c8db04212a2e24be1502f0d991465241d016
Aleixo-Dev <nicolasaleixo2020@gmail.com>
2024-05-02 11:33:39 -0300

    feat: change database version

--- app/src/main/java/com/nicolas/freegames/data/local/AppDatabase.kt
+++ app/src/main/java/com/nicolas/freegames/data/local/AppDatabase.kt
@@ -3,7 +3,7 @@ package com.nicolas.freegames.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [ModelGameEntity::class], version = 1)
+@Database(entities = [ModelGameEntity::class], version = 2)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun modelGameDao(): ModelGameDao
 }
