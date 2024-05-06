1052f47949c9e3a6c13c701b928603bddd9b4806
Aleixo-Dev <nicolasaleixo2020@gmail.com>
2024-05-06 12:01:15 -0300

    style: adjust function's return space

--- app/src/main/java/com/nicolas/freegames/data/local/datasource/GameLocalDataSourceImpl.kt
+++ app/src/main/java/com/nicolas/freegames/data/local/datasource/GameLocalDataSourceImpl.kt
@@ -2,10 +2,18 @@ package com.nicolas.freegames.data.local.datasource
 
 import com.nicolas.freegames.data.local.dao.ModelGameDao
 import com.nicolas.freegames.data.local.entities.GameEntity
+import javax.inject.Inject
 
-class GameLocalDataSourceImpl(private val gameDao: ModelGameDao) : GameLocalDataSource {
+class GameLocalDataSourceImpl @Inject constructor(
+    private val gameDao: ModelGameDao
+) : GameLocalDataSource {
 
-    override suspend fun create(gameEntity: List<GameEntity>) = gameDao.insert(gameEntity)
+    override suspend fun create(gameEntity: List<GameEntity>) {
+        gameDao.insert(gameEntity)
+    }
 
-    override suspend fun getAll() = gameDao.getData()
+    override suspend fun getAll(): List<GameEntity> {
+        val entity = gameDao.getData()
+        return entity
+    }
 }
